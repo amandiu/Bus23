ed3d6ab82b8da29b6aca6797915a880da4603619
Md.Amanullah Akon <79790568+amandiu@users.noreply.github.com>
2023-03-04 13:54:13 +0600

    all ok

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>I am a strong boy</Text>
+      <StatusBar
+        barStyle="light-content"
+        translucent={true}
+        backgroundColor="transparent"
+      />
+      <Navigation />
     </View>
   );
 }
 
+
+const Navigation = () => {
+  {
+    /*
+    
+    const MyTheme = {
+      ...DefaultTheme,
+      colors: {
+        ...DefaultTheme.colors,
+        primary: 'rgb(255, 45, 85)',
+        background: 'transparent'
+      },
+    };
+
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator>
+        <stack.screen/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    */
+  }
+  return (
+    <NavigationContainer>
+     
+    </NavigationContainer>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
